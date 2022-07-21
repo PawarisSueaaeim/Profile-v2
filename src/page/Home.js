6f66befd5481ad4d9a3f54f74280eb21d8160dc4
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-07-21 16:50:59 +0700

    home-page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Content.css";
+import ReactTypingEffect from "react-typing-effect";
 
 export default function Home() {
   return (
@@ -7,13 +8,31 @@ export default function Home() {
       <div className="container">
         <h3>Home</h3>
         <div className="content-box">
-          <div className="row">
-            <div className="col">
-              
-            </div>
-            <div className="col">
-
-            </div>
+          <div className="left-side">
+            <ReactTypingEffect
+              className="typing"
+              text={[
+                "Hello, My name is Pawaris Sueaaeim Computer Engineering",
+              ]}
+              speed={80}
+              eraseDelay={500}
+            />
+          </div>
+          <div className="right-side">
+            <h2>About me</h2>
+            <p>♦ Gender: Male</p>
+            <p>♦ National: Thai</p>
+            <p>♦ Address: Sukhuthai</p>
+            <p>♦ Thai: Native</p>
+            <p>♦ English: Fair</p>
+            <p>♦ Japan: A little bit</p>
+            <p>
+              Hello, My name is Pawaris Suea-aeim nickname Pleum, I'm 23 years
+              old, I'm graduated from Chiang Mai University faculty computer
+              engineering, Currently living in Rayong and working as a System
+              Engineer/IT Support at The amata city, Is a company related to the
+              production of auto parts.
+            </p>
           </div>
         </div>
       </div>
