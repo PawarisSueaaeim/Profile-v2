a66e83ffcf42433dd8e65d22106a9041246edb77
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-08-08 15:46:58 +0700

    update comment page

--- src/page/Comment.js
+++ src/page/Comment.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import '../Content.css'
+import React from "react";
+import "../Content.css";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
 
 export default function Comment() {
   return (
-    <div className="content-bg">Comment</div>
-  )
+    <div className="content-bg">
+      <div className="container">
+        <h4>Comment</h4>
+        <div className="content-box">
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Comment for me</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
