4366252fdf6c0dfe5b1813f5a40b561dd2464e3b
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-08-10 16:49:34 +0700

    comment update

--- src/page/Comment.js
+++ src/page/Comment.js
@@ -4,21 +4,28 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { db } from "../db";
 import { collection, getDocs, addDoc, setDoc, doc } from "firebase/firestore";
+import { query, where, onSnapshot } from "firebase/firestore";
+import * as FaIcons from "react-icons/fa";
 
-const ul = document.querySelector('ul')
-getDocs(collection(db, "comment")).then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
+// getDocs(collection(db, "comment")).then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     const data = doc.data();
+//     const li = document.createElement("li");
+//     li.textContent = `${data.comment}: ${data.like}`;
+//     document.getElementById("comment-list").appendChild(li);
 
-    const id = doc.id;
-    const data = doc.data();
-    const li = document.createElement("li");
-    li.textContent = `${data.comment}: ${data.like}`;
-    document.getElementById('comment-list').appendChild(li);
+//     console.log(data)
+//   });
+// });
 
-    console.log(doc.id, doc.data());
-  });
-});
-
+const data = [
+  {
+    comment: "good",
+  },
+  {
+    comment: "nice",
+  },
+];
 
 export default function Comment() {
   return (
@@ -27,12 +34,28 @@ export default function Comment() {
         <h4>Comment</h4>
         <div className="content-box">
           <form id="comment">
-            <label>Comment for me</label><br/>
-            <input type="text" id="comment" placeholder="Comment..." className="comment-input mt-3"/><br/>
-            <button type="submit" id="btn" className="btn btn-primary mt-3">Submit</button>
+            <label>Comment for me</label>
+            <br />
+            <input
+              type="text"
+              id="comment"
+              placeholder="Comment..."
+              className="comment-input mt-3"
+            />
+            <br />
+            <button type="submit" id="btn" className="btn btn-primary mt-3">
+              Submit
+            </button>
           </form>
           <div className="data-list mt-5">
             <ul id="comment-list">
+              {data.map((data, index) => {
+                return (
+                  <li key={index}>
+                    {data.comment}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
