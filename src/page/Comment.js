d9ce70d566cb3679d3e1bb8e645c2b100414a675
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-08-11 11:14:23 +0700

    form data complete

--- src/page/Comment.js
+++ src/page/Comment.js
@@ -6,41 +6,40 @@ import { db } from "../db";
 import { collection, getDocs, addDoc, setDoc, doc } from "firebase/firestore";
 import { query, where, onSnapshot } from "firebase/firestore";
 import * as FaIcons from "react-icons/fa";
+import { useState } from "react";
 
-// getDocs(collection(db, "comment")).then((querySnapshot) => {
-//   querySnapshot.forEach((doc) => {
-//     const data = doc.data();
-//     const li = document.createElement("li");
-//     li.textContent = `${data.comment}: ${data.like}`;
-//     document.getElementById("comment-list").appendChild(li);
-
-//     console.log(data)
-//   });
-// });
-
-const data = [
-  {
-    comment: "good",
-  },
-  {
-    comment: "nice",
-  },
-];
 
 export default function Comment() {
+
+  const [comment, setComment] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const data = {
+      comment: comment,
+    }
+    console.log(data);
+    setComment('');
+  }
+
   return (
     <div className="content-bg">
       <div className="container">
         <h4>Comment</h4>
         <div className="content-box">
-          <form id="comment">
-            <label>Comment for me</label>
+          <form id="comment" onSubmit={handleSubmit}>
+            <label htmlFor="comment">Comment for me</label>
             <br />
             <input
               type="text"
               id="comment"
               placeholder="Comment..."
               className="comment-input mt-3"
+              onChange={ (e) => {
+                //dom
+                setComment(e.target.value);
+              }}
+              value={comment}
             />
             <br />
             <button type="submit" id="btn" className="btn btn-primary mt-3">
@@ -49,13 +48,13 @@ export default function Comment() {
           </form>
           <div className="data-list mt-5">
             <ul id="comment-list">
-              {data.map((data, index) => {
+              {/* {data.map((data, index) => {
                 return (
                   <li key={index}>
                     {data.comment}
                   </li>
                 );
-              })}
+              })} */}
             </ul>
           </div>
         </div>
