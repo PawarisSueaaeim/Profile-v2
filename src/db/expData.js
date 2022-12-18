9525495623b5cd8a0d7096178ca4a668e8b1a108
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2022-12-18 14:33:27 +0700

    mock data exp

--- src/db/expData.js
+++ src/db/expData.js
@@ -3,7 +3,30 @@ export const expData = [
         position: "Bachelor's degree of computer engineering",
         time: "August 2017 - May 2021",
         title: "Computer engineering",
-        detail1: "Basic engineering - Drawing/Calculus/Physics",
-        detail2: "Basic automation - Programming/Mechatronics",
-    }
+        detail: [
+            "Basic engineering - Drawing/Calculus/Physics",
+            "Basic automation - Programming/Mechatronics",
+            "Manage plan and tasks of programming project or network project ",
+            "Design/Develop hardwere and softwere Learn to new thing of technology such as image processing computer visions"
+        ]
+    },{
+        position: "System Engineer/IT Support",
+        time: "October 2021 - Current",
+        title: "Computer engineering",
+        detail: [
+            "Responsibility for Admin & Support.",
+            "Maintain and monitoring IT regulation regarding IT security",
+            "IT vendor management for project and maintenance. Responsibility for Network",
+            "Manage plan and tasks of network and IT infrastructure team"
+        ] 
+    },{
+        position: "Software Tester",
+        time: "Sep 2022 - Present",
+        title: "Create test case and Support customer",
+        detail: [
+            "Create test case.",
+            "Test and report issue.",
+            "Fix problems with users.",
+        ] 
+    },
 ];
