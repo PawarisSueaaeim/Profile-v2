8b3a3aec3c8d68ed2bad310f1fda4389b0e866a6
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2022-12-21 14:17:07 +0700

    dont know

--- src/component/subComponents/CardExp.js
+++ src/component/subComponents/CardExp.js
@@ -1,20 +1,22 @@
 function CardExp(props) {
 
-    console.log("this is a CardExp", props);
-    console.log("this is a props children", props.children);
-
-    const {isActive} = props;
+    // console.log("this is a CardExp", props);
+    // console.log("this is a props children", props.children);
 
     const getClassName = () => {
-        props.children.map(() => {
-            
-        });
+        if (props.children.map((items,index) => {
+            return index
+        })%2 === 0){
+            return "container-exp left";
+        }else{
+            return "container-exp right";
+        }
     };
 
-    const classes = props.className;
+    const classes = props.className + getClassName();
 
     return (
-        <div className={classes}>
+        <div className={getClassName()}>
             {props.children}
         </div>
     );
